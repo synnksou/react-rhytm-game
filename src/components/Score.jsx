1cdcee5f27743e15f50c61bb42dcf3f0ba1bfbad
Antoine <heinrich.antoine.syn@gmail.com>
2023-04-12 13:03:09 +0200

    feat: add music and sound

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { HStack, Text } from "@chakra-ui/layout";
+import { AUDIO_PATH_PLAYER_ARRAY } from "../utils";
 
-const Score = ({ score }) => {
-  const [pastScore, setPastScore] = useState(0);
-
-  useEffect(() => {
-    if (score > pastScore) {
-      setPastScore(score - pastScore);
-    }
-  }, [score]);
+const Score = ({ score, prevScore }) => {
+  const diffScore = score - prevScore;
 
   return (
     <HStack>
       <Text color="white" fontSize="3xl">
         Votre Score {score}{" "}
       </Text>{" "}
-      {score > pastScore && (
+      {score > prevScore && prevScore != 0 && (
         <Text fontSize="3xl" color="green.400">
-          + {pastScore}{" "}
+          + {diffScore}{" "}
         </Text>
       )}
     </HStack>
